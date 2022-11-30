cbdb0bb6e74a939d4cc16870f85b7a9ffd2e9b19
昱緯 <csabc993611@Gmail.com>
2022-12-01 00:47:46 +0800

    add file

---
+++ Demo/Controllers/Api/DemoController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [Route("/info")]
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(true);
+        }
+    }
+}
